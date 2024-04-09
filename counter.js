75de23d2b874a90d08e5a8652edda48f789e67ca
FocusFine89 <niivanov02@gmail.com>
2024-04-09 17:54:32 +0200

    changes

--- counter.js
+++ counter.js
@@ -3,11 +3,16 @@ const p = document.querySelector("p");
 const TIMER_MEMORY = "timerMemory";
 let counter = 0;
 window.addEventListener("DOMContentLoaded", () => {
+  const counterStorage = sessionStorage.getItem(TIMER_MEMORY);
+  if (counterStorage) {
+    counter = JSON.parse(counterStorage);
+    p.innerText = counter;
+  }
   setInterval(() => {
     counter++;
     sessionStorage.setItem(TIMER_MEMORY, counter);
     const counterStorage = JSON.parse(sessionStorage.getItem(TIMER_MEMORY));
-    p.innerText = `${counterStorage}`;
+    p.innerText = counterStorage;
   }, 1000);
 
   btnReset.addEventListener("click", () => {
